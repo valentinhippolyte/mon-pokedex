27fcbc701e7a232863692bd85b529877982c9daa
Valentin Hippolyte <valenthippo@gmail.com>
2024-01-12 12:16:53 +0100

    delete Pokedex dataClass + some style added

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,17 +1,25 @@
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import moe.tlaster.precompose.navigation.rememberNavigator
 import network.repository.PokemonRepository
 import network.repository.PokemonTypeRepository
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 
 @Composable
--- shared/src/commonMain/kotlin/dataClass/Pokedex.kt
+++
@@ -1,6 +0,0 @@
-package dataClass
-
-@kotlinx.serialization.Serializable
-data class Pokedex(
-    var pokemons: List<Pokemon>
-)
